31fcdae68956d881a5e65e4e502f3495f0822c24
Amir <aamirvand97@gmail.com>
2019-11-13 05:35:50 -0800

    Removed some uneecessary lines that use to have function but now don't, made UI look more pretty. Generator works, user can copy given password manually and set length that they desire.Was too lazy to add a copy/pasta button.

--- script.js
+++ script.js
@@ -27,12 +27,16 @@ function generate(){
 
 // Was a copy and paste to get the slider to work, uncertain on what values need to be set
 document.getElementById("slider").oninput = function(){
-
   if(document.getElementById("slider").value > 0){
       document.getElementById("length").innerHTML = "Length: " + document.getElementById("slider").value;
   }
   else{
       document.getElementById("length").innerHTML = "Length: 8";
   }
+}
 
+function copyPassword(){
+  document.getElementById("display").select();
+  document.execCommand("Copy");
+  alert("Password copied to clipboard!");
 }
