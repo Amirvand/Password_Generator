f39eb56d94e2a38c1acd194e1b5c071f5e22eb49
Amir <aamirvand97@gmail.com>
2019-11-13 05:28:36 -0800

    made password properly generate in the box, was going to add check boxes but thought it would just be easier to make the password extremely difficult from the get-go.

--- script.js
+++ script.js
@@ -9,6 +9,23 @@ var symbols = ["`~!@#$%^&*()-_=+[}{]';:.>,</?|"]
 //Array of Special Symbols
 var specialSymbols = ["ÇübéâäàåçêëèïîìæÆôöòûùÿ¢£¥ƒáíóúñÑ¿¬½¼¡«»¦ßµ±°•·²€„…†‡ˆ‰Š‹Œ™š›œŸ¨©®¯³´¸¹¾ÀÁÂÃÄÅÈÉÊËÌÍÎÏÐÒÓÔÕÖ×ØÙÚÛÜÝÞãðõ÷øüýþ"]
 
+function generate(){
+  //set length to the password
+  let difficulty = document.getElementById("slider").value;
+  //possible password values
+  let values = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`~!@#$%^&*()-_=+[}{]';:.>,</?|ÇübéâäàåçêëèïîìæÆôöòûùÿ¢£¥ƒáíóúñÑ¿¬½¼¡«»¦ßµ±°•·²€„…†‡ˆ‰Š‹Œ™š›œŸ¨©®¯³´¸¹¾ÀÁÂÃÄÅÈÉÊËÌÍÎÏÐÒÓÔÕÖ×ØÙÚÛÜÝÞãðõ÷øüýþ";
+  let password = "";
+  //create for loop to choose password characters
+  for(var i = 0; i <= difficulty; i++){
+      password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+  }
+  //add password to textbox/display area
+  document.getElementById("display").value = password;
+  //add password to previously generated passwords section
+  document.getElementById("lastNums").innerHTML += password + "<br />";
+}
+
+// Was a copy and paste to get the slider to work, uncertain on what values need to be set
 document.getElementById("slider").oninput = function(){
 
   if(document.getElementById("slider").value > 0){
