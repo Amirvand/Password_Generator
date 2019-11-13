ad963656a79f16b78124fec242b613898448d0f4
Amir <aamirvand97@gmail.com>
2019-11-13 05:17:23 -0800

    made UI look a lot more pretty, added slider (uncertain if this will work), added buttons to  and  password

--- script.js
+++ script.js
@@ -1,47 +1,34 @@
 //Array of lowercase characters
-var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var lowercase = ["abcdefghijklmnopqrstuvwxyz"]
 //Array of uppercase characters
-var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var uppercase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
 //Array of Numbers
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var numbers = ["01234566789"]
 //Array of Symbols
-var symbols = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "}", "{", "]", "'", ";", ":", ".", ">", ",", "<", "/", "?", "|"]
+var symbols = ["`~!@#$%^&*()-_=+[}{]';:.>,</?|"]
 //Array of Special Symbols
-var specialSymbols = ["Ç","ü", "b", "é", "â", "äàåçêëèïîìæÆôöòûùÿ¢£¥ƒáíóúñÑ¿¬½¼¡«»¦ßµ±°•·²€„…†‡ˆ‰Š‹Œ™š›œŸ¨©®¯³´¸¹¾ÀÁÂÃÄÅÈÉÊËÌÍÎÏÐÒÓÔÕÖ×ØÙÚÛÜÝÞãðõ÷øüýþ"]
+var specialSymbols = ["ÇübéâäàåçêëèïîìæÆôöòûùÿ¢£¥ƒáíóúñÑ¿¬½¼¡«»¦ßµ±°•·²€„…†‡ˆ‰Š‹Œ™š›œŸ¨©®¯³´¸¹¾ÀÁÂÃÄÅÈÉÊËÌÍÎÏÐÒÓÔÕÖ×ØÙÚÛÜÝÞãðõ÷øüýþ"]
 
-function () {
-    var length = parseInt(prompt(
-      "How long do you want your password to be? Minimum : 8 , Maximimum : 128"
-    ));
-
-    if (isNaN(length) === true) {
-      alert("Password length must be provided as a number. Example : 1 , 43, 92");
-      return;
-    }
-  
-    if (length < 8) {
-      alert("The password length you have chosen is too short, please pick a length that is above 8 characters.");
-      return;
-    }
-
-    if (length > 128) {
-      alert("Your password length is too long, please pick a password length that is below 129 characters.");
-      return;
-    }
-  
-    return passwordOptions;
+function generateRandomPassword (passwordLength) {
+  var outputPassword = "";
+  var allPossibleChars  = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*()_+-=";
+  for (var i = 0; i < passwordLength; i++) {
+      outputPassword += allPossibleChars.charAt(Math.floor(Math.random() * allPossibleChars.length));
   }
-
-  // Function for getting a random element from an array
-function getRandom(arr) {
-  var randIndex = Math.floor(Math.random() * arr.length);
-  var randElement = arr[randIndex];
-
-  return randElement;
+  return outputPassword;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function myFunction() {
+  var copyText = document.getElementById("output");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+  
+  var tooltip = document.getElementById("myTooltip");
+  tooltip.innerHTML = "Copied: " + copyText.value;
+}
 
-// Add event listener to copy button
-copyBtn.addEventListener("click", copyToClipboard);
+function outFunc() {
+  var tooltip = document.getElementById("myTooltip");
+  tooltip.innerHTML = "Copy to clipboard";
+}
